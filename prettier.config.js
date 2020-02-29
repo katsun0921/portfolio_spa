4954ce6f2b52bbc661d707c50efcc3cb7181be06
katsun0921 <s.katsumasa@gmail.com>
2020-02-29 23:01:59 +0900

    gatsbyの環境設定を構築 prettirの設定を追加 gatsbyのデフォルトコンポーネントを追加

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf', //End of Line
+  semi: false, // Print semicolons at the ends of statements.
+  tabWidth: 2, // Specify the number of spaces per indentation-level.
+  trailingComma: 'all', // Print trailing commas wherever possible when multi-line.
+  singleQuote: true, // Use single quotes instead of double quotes.
+  jsxSingleQuote: false, // Use single quotes instead of double quotes in JSX.
+  bracketSpacing: true, // Print spaces between brackets in object literals.
+  arrowParens: 'always', // Include parentheses around a sole arrow function parameter.
+}
