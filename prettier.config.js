9c83ced76f8c2e298f93bf2105768d7744b5e10f
katsun0921 <s.katsumasa@gmail.com>
2020-03-01 09:41:54 +0900

    prettierの設定のコメントを日本語へ変換

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  endOfLine: 'lf', //End of Line
-  semi: false, // Print semicolons at the ends of statements.
-  tabWidth: 2, // Specify the number of spaces per indentation-level.
-  trailingComma: 'all', // Print trailing commas wherever possible when multi-line.
-  singleQuote: true, // Use single quotes instead of double quotes.
-  jsxSingleQuote: false, // Use single quotes instead of double quotes in JSX.
-  bracketSpacing: true, // Print spaces between brackets in object literals.
-  arrowParens: 'always', // Include parentheses around a sole arrow function parameter.
+  printWidth: 80, // 行の折り返し地点
+  endOfLine: 'lf', // 改行コード
+  useTabs: false, // インデントはスペース
+  semi: false, // 文末のセミコロンの有無
+  tabWidth: 2, // インデント幅を設定
+  trailingComma: 'es5', // ES5 objects, arraysなどで末尾にcommasをつける
+  singleQuote: true, // シングルクォートに統一
+  jsxSingleQuote: false, // JSXの場合、ダブルクォート
+  bracketSpacing: true, // オブジェクトの{}の前後にスペースを入れる
+  arrowParens: 'always', // ES6のアロー関数の場合、引数は()をつける
 }
