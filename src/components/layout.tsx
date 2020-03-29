14b1165d75366775bfd8d571c687d827c8aa5fb2
katsun0921 <s.katsumasa@gmail.com>
2020-03-29 14:56:17 +0900

    不要なimportを削除

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,6 @@
 
 import React, { ReactNode } from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import GlobalStyle from 'styles/layout'
 interface Prop {
