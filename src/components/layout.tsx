e98f882527f5c44cafef40741b49d06260becf48
katsun0921 <s.katsumasa@gmail.com>
2020-04-08 17:43:01 +0900

    scssファイルとimagesファイルをassetsへ移動

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import GlobalStyle from '../styles/layout'
-import 'styles/scss/layout/_container.scss'
+import 'scss/layout/_container.scss'
 import bgImg from 'images/main.jpg'
 import bgImgSp from 'images/main_sp.jpg'
 import { VAR_MQ } from '../styles/variables'
